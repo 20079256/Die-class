b05f77ea2ccdec2891e447daf8c26c83a94f99bd
Student-Mohame2 <95230222+20079256@users.noreply.github.com>
2021-11-29 14:25:22 +0000

    Initial commit

---
+++ Die.java
@@ -0,0 +1,44 @@
+public class Die
+{
+private int value;
+private int numSides;
+
+public Die(int v, int n)
+{
+setValue(v);
+setNumSides(n);
+}
+
+public Die()
+{
+  this(1,6);
+}
+
+public int getValue()
+{
+  return value;
+}
+public int getNumSides()
+{
+  return numSides;
+}
+
+public void setValue(int v)
+{
+  value=v;
+}
+public void setNumSides(int ns)
+{
+  numSides=ns;
+}
+
+public int roll()
+{
+  return (1+((int)(numSides*Math.random())));
+}
+
+public String toString()
+{
+  return "Value: "+value+"NumSides: "+numSides;
+}
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    Die d1=new Die();
+    System.out.println(d1);
+    System.out.println(d1.roll());
+    System.out.println(d1);
+  }
+}
