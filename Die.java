b3864726974b22c7ca7aab941b60221022a1fe03
Student: Mohamed Mohamed <95230222+20079256@users.noreply.github.com>
2022-01-12 14:48:34 +0000

    TWODIEGAMES

--- Die.java
+++ Die.java
@@ -2,11 +2,17 @@ public class Die
 {
 private int value;
 private int numSides;
+private static int id; //like a counter shared between classes
+private int dieId;
+
+
 
 public Die(int v, int n)
 {
 setValue(v);
 setNumSides(n);
+id++;
+setDieId(id);
 }
 
 public Die()
@@ -27,18 +33,42 @@ public void setValue(int v)
 {
   value=v;
 }
-public void setNumSides(int ns)
+public void setNumSides(int n)
 {
-  numSides=ns;
+  numSides=n;
+}
+
+public void setDieId(int id)
+{
+  dieId = id;
+}
+
+public int getDieId()
+{
+  return dieId;
 }
 
 public int roll()
 {
+  int value = (1+((int)(numSides*Math.random())));
+this.setValue(value);
   return (1+((int)(numSides*Math.random())));
+}
+
+public boolean equals (Die d)
+{
+  if(this.getValue() == d.getValue())
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 public String toString()
 {
-  return "Value: "+value+"NumSides: "+numSides;
+  return "Value: "+value+"\nNumSides: "+numSides+ "\ndie Id " + dieId;
 }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
+import java.util.*;
 class Main {
   public static void main(String[] args) {
     Die d1=new Die();
+    Die d2 = new Die();
+    Scanner it = new Scanner (System.in);
+    boolean ans=d1.equals(d2);
+    System.out.println("Are d1 and d2 equals? "+ans);
     System.out.println(d1);
     System.out.println(d1.roll());
     System.out.println(d1);
+    System.out.println("Are d1 and d2 equals? "+d1.equals(d2));
+    Die d3 = new Die (1,20);
+    d3.roll();
+    System.out.println("d3: "+d3);
+    d3.roll();
+    System.out.println("d3: "+d3);
+    System.out.println("Are d1 and d3 equal? "+d1.equals(d3));
+    System.out.println("Please enter the amount of rolls");
+int num =it.nextInt();
+TwoDieGames p = new TwoDieGames(num);
+p.snakeEyes();
   }
-}
+} 
